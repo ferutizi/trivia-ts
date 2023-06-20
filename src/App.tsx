506ed0a4a92057ea8e436550b49eb768c5dffe2e
ferutizi <fer.utizi@gmail.com>
2023-06-20 16:45:22 -0300

    points state, fetch next question, block select multiple answers

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { type Question } from './types'
 function App (): JSX.Element {
   const [questions, setQuestions] = useState<Question[]>([])
   const [answers, setAnswers] = useState<string[]>([])
+  const [selectedAnswer, setSelectedAnswer] = useState<number>(0)
+  const [points, setPoints] = useState<number>(0)
 
   useEffect(() => {
     void getQuizz()
@@ -15,6 +17,7 @@ function App (): JSX.Element {
     const data = await res.json()
     setQuestions(data)
     disorderAnswers(data[0].incorrectAnswers, data[0].correctAnswer)
+    setSelectedAnswer(0)
   }
 
   const disorderAnswers = (incorrectAnswers: string[], correctAnswer: string): void => {
@@ -29,28 +32,31 @@ function App (): JSX.Element {
       return 0
     })
     console.log(correctAnswer)
-    console.log(newAnswers)
     setAnswers(newAnswers)
   }
 
   const isCorrectAnswer = (question: Question, answerSelected: string): void => {
+    setSelectedAnswer(selectedAnswer + 1)
     if (answerSelected === question.correctAnswer) {
       console.log(true)
+      setPoints(points + 1)
     } else {
       console.log(false)
     }
+    void getQuizz()
   }
 
   return (
     <>
       <h1>Trivia</h1>
+      <p>{points}</p>
       <ul>
         {questions.map(item =>
           <div key={item.id}>
             <h3>{item.category}</h3>
             <p>{item.question}</p>
             {answers.map((i: string, index: any) =>
-              <p key={index} onClick={() => { isCorrectAnswer(item, i) }}>{i}</p>
+              <p key={index} onClick={() => { selectedAnswer === 0 && isCorrectAnswer(item, i) }}>{i}</p>
             )}
           </div>
         )}
